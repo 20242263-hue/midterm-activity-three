c69282e36cb43f6c00a5ed7ea359244685c1e227
taglinao_p <taglinao_p@ca01.ub.lan>
2026-03-16 12:40:00 +0800

    Created navigation component and setup the router.

---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home: React.FC = () => {
+  return (
+    <div>
+      <h1>Welcome to the Student Registration System</h1>
+      <p>Click on "Register" to create a new student.</p>
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/pages/Register.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Register: React.FC = () => {
+  return (
+    <div>
+      <h1>Student Registration</h1>
+      <p>Fill in the form to register a new student.</p>
+    </div>
+  );
+};
+
+export default Register;
